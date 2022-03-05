b716dcc36789d18e03ae88d8416cf0a8d29b31b7
isudfv <isudfv@outlook.com>
2022-03-05 23:42:56 +0800

    invode_result

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -4,6 +4,7 @@
 
 #ifndef THREAD_POOL_THREADPOOL_H
 #define THREAD_POOL_THREADPOOL_H
+
 #include "ConQueue.h"
 #include <functional>
 #include <thread>
@@ -27,7 +28,7 @@ private:
     public:
         explicit ThreadWorker(ThreadPool *pool/*, const int id*/) : _pool(pool)/*, _id(id)*/ {}
 
-        void operator () () {
+        void operator()() {
 //            std::function<void ()> func;
 
             bool success = false;
@@ -36,7 +37,7 @@ private:
                 {
                     std::unique_lock<std::mutex> lock(_pool->_mutex); // 单个线程获取任务时加锁
 
-                    if (_pool->_q.empty()){
+                    if (_pool->_q.empty()) {
                         std::cout << "blocked\n";
                         _pool->_cv.wait(lock);
                     }
@@ -56,7 +57,7 @@ public:
     explicit ThreadPool(const int _n_threads) : _shutdown(false) {
         _threads.resize(_n_threads);
 //        for (int i = 0; i < _threads.size(); ++ i)
-        for (auto & p : _threads) {
+        for (auto &p: _threads) {
             // 初始化线程，任务队列目前为空，自然阻塞
             p = std::jthread(ThreadWorker(this));
         }
@@ -66,16 +67,16 @@ public:
 
     ThreadPool(ThreadPool &&) = delete;
 
-    ThreadPool &operator = (const ThreadPool &) = delete;
+    ThreadPool &operator=(const ThreadPool &) = delete;
 
-    ThreadPool &operator = (ThreadPool &&) = delete;
+    ThreadPool &operator=(ThreadPool &&) = delete;
 
     void shutdown() {
         _shutdown = true;
         // 唤醒并 join 所有线程
         _cv.notify_all();
 
-        for (auto &p : _threads) {
+        for (auto &p: _threads) {
             if (p.joinable())
                 p.join();
         }
@@ -89,18 +90,53 @@ public:
         // 用 std::packaged_task 封装任务以异步操作结果
         auto task_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
         // wrapper是为了统一调用 func()
-        std::function<void ()> wrapper_func = [task_ptr] {
+/*
+        std::function<void()> wrapper_func = [task_ptr] {
             (*task_ptr)();
         };
-        // 添加到任务队列
-        _q.push(wrapper_func);
+*/
+        // 添加到任务队列，用 lambda 替换 wrapper
+        _q.emplace([task_ptr] {
+            (*task_ptr)();
+        });
+//        _q.push(wrapper_func);
         // 唤醒一个线程
         _cv.notify_one();
 
         return task_ptr->get_future();
     }
 
+/*
+    template<typename F, typename... A,
+            typename = std::enable_if_t<std::is_void_v<
+                    std::invoke_result_t<std::decay_t<F>, std::decay_t<A>...>>>>
+    std::future<bool> Submit(const F &task, const A &&... args) {
+        auto promise = std::make_shared<std::promise<bool>>();
+        auto future = promise->get_future();
+        PushTask([task, args..., promise] {
+            task(args...);
+            promise->set_value(true);
+        });
+        return future;
+    }
+*/
+
+/*
+    template<
+            typename F, typename... A,
+            typename R = std::invoke_result_t<std::decay_t<F>, std::decay_t<A>...>,
+            typename = std::enable_if_t<!std::is_void_v<R>>>
+    std::future<R> Submit(const F &task, const A &&... args) {
+        auto promise = std::make_shared<std::promise<R>>();
+        auto future = promise->get_future();
+        _q.emplace([task, args..., promise] {
+            promise->set_value(task(args...));
+        });
+        return future;
+    }
+
 };
+*/
 
 
 #endif //THREAD_POOL_THREADPOOL_H
